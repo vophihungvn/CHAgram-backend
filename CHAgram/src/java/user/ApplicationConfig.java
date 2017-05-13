5b32731a9fee963c59ce1c04658f27a6c2c6a2e8
Vu Tuan Anh <tuananh05253@gmail.com>
2017-05-13 15:24:48 +0700

    Revert "Add Image Processing service"
    This reverts commit e2935432527f03b3e33aff99eb5d660eebce5278.

--- CHAgram/src/java/user/ApplicationConfig.java
+++ CHAgram/src/java/user/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(user.UserResource.class);
         resources.add(user.UsersResource.class);
     }
     
